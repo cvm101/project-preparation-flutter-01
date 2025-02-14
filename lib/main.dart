77101608c3335d912945ef314a02107e1beec4af
SHIVAM KUMAR <cvm@SHIVAMs-MacBook-Air.local>
2025-02-14 22:44:49 +0530

    stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,37 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-class MyApp extends StatelessWidget{
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int counter = 0;
+
+  void incrementCounter() {
+    setState(() {
+      counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("This is my first project"),
+          backgroundColor: Colors.red,
         ),
         body: Center(
-          child: Text("So this is how we center an element"),
+          child: Text(
+            counter.toString(), // Convert int to string
+            style: TextStyle(fontSize: 30), // Optional: Add some styling
+          ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print("button pressed");
-          },
+          onPressed: incrementCounter, // Call the method correctly
           child: Icon(Icons.add),
         ),
       ),
